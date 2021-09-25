08eb81a05845b3fb1e4248ba4f18d97604bebcbd
Praphulla Kherade <prafullkherade9@gmail.com>
2021-09-25 19:51:12 +0530

    Prime Number or Not

---
+++ PrimeNumber.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+read -p "Enter a number : " n
+base=0;
+a=0;
+b=n/2;
+for (( counter=2; counter<=b; counter++ ))
+do
+        if [ $(($n%$counter)) -eq 0 ]
+        then
+                echo "$n is not prime"
+        else
+                echo "$n is prime"
+        fi
+done
